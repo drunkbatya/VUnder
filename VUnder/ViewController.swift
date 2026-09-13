a3a80287b934ce834eb73b4128ca9902b5562ce3
DrunkBatya <drunkbatya.js@gmail.com>
2026-09-13 14:28:21 +0300

    Initial Commit

---
+++ VUnder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VUnder
+//
+//  Created by drunkbatya on 13/09/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VUnderTests/VUnderTests.swift
@@ -0,0 +1,17 @@
+//
+//  VUnderTests.swift
+//  VUnderTests
+//
+//  Created by drunkbatya on 13/09/2026.
+//
+
+import Testing
+@testable import VUnder
+
+struct VUnderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
